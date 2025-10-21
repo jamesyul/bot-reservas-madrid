669ccec588f0ad3f14982fa616cdb9319657409a
Yul Cardenas <69341405+jamesyul@users.noreply.github.com>
2025-10-21 13:12:15 +0200

    Fix requirements

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+# ¡IMPORTANTE! Este archivo no debe subirse a GitHub.
+# Asegúrate de que ".config.py" está en tu .gitignore
+
+USERNAME = "yulcardaso@outlook.com"
+PASSWORD = "Decve6-benwud-muvmyn"
